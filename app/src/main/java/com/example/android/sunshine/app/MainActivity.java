3dce7521acbbf5585b2cec0647e064e398ba3850
Pablo Arango <pabloaran17@gmail.com>
2016-03-28 02:58:54 -0400

    initialize list view adapter

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -72,6 +73,17 @@ public class MainActivity extends ActionBarActivity {
             };
 
             ArrayList<String> fakeForecastData = new ArrayList<>(Arrays.asList(fakeForecastDataString));
+
+            ArrayAdapter<String> forecastItemListAdapter =
+                    new ArrayAdapter<String>(
+                            // Parent activity for context
+                            getActivity(),
+                            // layout for list
+                            R.layout.list_item_forecast,
+                            //textview to populate
+                            R.id.list_item_forecast_textview,
+                            // data for textview
+                            fakeForecastData);
 
             return rootView;
         }
