201e053b064e4f5afa949501c0a6f5c93b4b6ac2
Pablo Arango <pabloaran17@gmail.com>
2016-04-02 10:52:16 -0400

    inflate options menu

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -30,18 +33,45 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("32601");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         String[] fakeForecastDataString = {
                 "Today - Rainy - 78/60",
-                "Tomorrow - Sunny 88/65",
-                "Weds - Sunny 88/66",
-                "Thurs - Foggy 88/66",
-                "Fri - Cloudy 88/66",
-                "Sat - Sunny 88/66",
-                "Sun - Sunny 88/66"
+                "Tomorrow - Sunny - 88/65",
+                "Weds - Sunny - 88/66",
+                "Thurs - Foggy - 88/66",
+                "Fri - Cloudy - 88/66",
+                "Sat - Sunny - 88/66",
+                "Sun - Sunny - 88/66"
         };
 
         ArrayList<String> fakeForecastData = new ArrayList<>(Arrays.asList(fakeForecastDataString));
@@ -70,6 +100,12 @@ public class ForecastFragment extends Fragment {
 
         @Override
         protected String doInBackground(String... strings) {
+
+            if(strings.length == 0){
+                return null;
+            }
+
+
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
@@ -77,6 +113,9 @@ public class ForecastFragment extends Fragment {
 
             // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
+            String format = "json";
+            String units = 'metric';
+            int numDays = 7;
 
             try {
                 // Construct the URL for the OpenWeatherMap query
@@ -113,6 +152,7 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Forecast JSON String: "+ forecastJsonStr);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
